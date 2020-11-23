ddc8f6506f7682bdb7b6db207a497baf2ed33bba
Arnelle Balane <arnellebalane@gmail.com>
2020-11-24 00:42:00 +0800

    Add ExpressJS app that renders index page

---
+++ .copy-paste/00-commands.md
@@ -0,0 +1,9 @@
+# ExpressJS App
+
+```
+npm init -y
+npm install express
+node app.js
+
+npm install express-handlebars
+```
---
+++ .copy-paste/05-express-handlebars.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.engine('html', handlebars());
+app.set('view engine', 'html');
+app.set('views', './pages');
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.render('index', { layout: false });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
